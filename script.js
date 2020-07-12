9d688e2cdd419e0dbb47501ceaf9b83386abef6f
Srishti1905 <68160449+Srishti1905@users.noreply.github.com>
2020-07-12 13:57:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 30;
-const apiKey = 'nnlKG1Ya2sv1JpMRWnvhmZrwXIbrO7Iv9zQFYSQFObI';
+const apiKey = 'API_KEY';
 const apiUrl= `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all imaes were loaded
